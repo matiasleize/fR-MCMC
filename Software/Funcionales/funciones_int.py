13d291aeaf3ad0d2e18a814154d6773dad830b3e
matiasleize <matias.leize@gmail.com>
2020-02-08 23:51:19 -0300

    Limpio un poco la funcion integrador, que tenia cosas en el medio innecesarias.

--- Software/Funcionales/funciones_int.py
+++ Software/Funcionales/funciones_int.py
@@ -3,42 +3,38 @@ Created on Sun Feb  2 13:28:48 2020
 
 @author: matias
 """
-from matplotlib import pyplot as plt
 
-from scipy.integrate import solve_ivp
 import numpy as np
+from matplotlib import pyplot as plt
+from scipy.integrate import solve_ivp
 from scipy.integrate import simps as simps
 import time
 
 from scipy.interpolate import interp1d
-from scipy.constants import c
-H_0 =  73.48
+from scipy.constants import c as c_luz
 
-
-
-def integrador(sistema_ec, cond_iniciales, parametros_modelo, z_inicial=0, z_final=3):    
+def integrador(sistema_ec, cond_iniciales, parametros_modelo,
+               z_inicial=0, z_final=3, cantidad_zs=2000, max_step=0.05): 
+    #cantidad_zs_ideal = 7000
+    # max_step_ideal = 0.005
+    
     '''Esta función integra el sistema de ecuaciones diferenciales entre 
     z_inicial y z_final, dadas las condiciones iniciales y los parámetros
     del modelo.'''
     [b,d,c,r_0,n] = parametros_modelo
-    # Resolvemos
-    sol = solve_ivp(sistema_ec, [z_inicial,z_final], cond_iniciales, max_step=0.01)
-    
+   
     # Integramos el vector v y calculamos el Hubble
-    zs = np.linspace(0,3,7000) #7000
+    zs = np.linspace(z_inicial,z_final,cantidad_zs) 
     hubbles = np.zeros(len(zs))
-    
-    lala = np.zeros(len(zs))
     
     t1 = time.time()
     for i in range(len(zs)):
-        zi = zs[0]
-        zf = zs[i]
-        sol = solve_ivp(sistema_ec, [zi,zf], cond_iniciales, max_step=0.05)      # 0.005
+        zf = zs[i] #''z final'' para cada paso de integracion
+        sol = solve_ivp(sistema_ec, [z_inicial,zf], cond_iniciales,
+                        max_step=max_step)      
     
-        int_v = simps((sol.y[2])/(1+sol.t),sol.t) 
-        lala[i] = int_v
-        hubbles[i]=(1+zf)**2 * np.e**(-int_v) # integro desde 0 a z, ya arreglado
+        int_v = simps((sol.y[2])/(1+sol.t),sol.t) # integro desde 0 a z 
+        hubbles[i]=(1+zf)**2 * np.e**(-int_v) 
     t2 = time.time()
     
     print('Duración {} minutos y {} segundos'.format(int((t2-t1)/60),
@@ -62,7 +58,7 @@ def plot_sol(solucion):
     plt.show()
     
 
-def magn_aparente_teorica(z,E,zhel,zcmb):
+def magn_aparente_teorica(z,E,zhel,zcmb,H_0=73.48):
     '''A partir de un array de redshift y un array de la magnitud E = H_0/H
     que salen de la integración numérica, se calcula el mu teórico que deviene
     del modelo. muth = 25 + 5 * log_{10}(d_L), 
@@ -70,7 +66,7 @@ def magn_aparente_teorica(z,E,zhel,zcmb):
     
     d_c=np.zeros(len(E)) #Distancia comovil
     for i in range (1, len(E)):
-        d_c[i] = 0.001*(c/H_0) * simps(1/E[:i],z[:i])
+        d_c[i] = 0.001*(c_luz/H_0) * simps(1/E[:i],z[:i])
         
     dc_int = interp1d(z,d_c) #Interpolamos 
     
