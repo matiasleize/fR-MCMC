33c82c168c002216d02b1d05d148dcb4c78d53ce
matiasleize <matias.leize@hotmail.com>
2020-03-07 00:10:33 -0300

    Agrego la funcion params_to_chi2_H0, que toma como parametro libre H0 en vez de como fijo.

--- Software/Funcionales/funciones_cronometros.py
+++ Software/Funcionales/funciones_cronometros.py
@@ -39,3 +39,20 @@ def params_to_chi2(cond_iniciales, theta, params_fijos, z_data, H_data, dH):
 
     chi = chi_2_cronometros(H_teo,H_data,dH)
     return chi
+
+def params_to_chi2_H0(cond_iniciales, theta, params_fijos, z_data, H_data, dH):
+    '''Dados los parámetros del modelo devuelve un chi2 para los datos de los
+    cronómetros cósmicos'''
+
+    [omega_m,b,H_0] = theta
+    n = params_fijos
+
+    ## Transformo los parametros fisicos en los del modelo:
+    c1,c2,r_hs = params_fisicos_to_modelo(omega_m,b)
+    params_modelo = [c1,r_hs,c2,n] #de la cruz: [b,c(*H0^2),d,r_0,n]
+    z,E = integrador(cond_iniciales, params_modelo)
+    H_int = interp1d(z,E)
+    H_teo = H_0 * H_int(z_data)
+
+    chi = chi_2_cronometros(H_teo,H_data,dH)
+    return chi
