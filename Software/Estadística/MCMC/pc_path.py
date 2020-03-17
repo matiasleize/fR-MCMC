4549648a3ffd619d8746134ddaa586d7918916bd
matiasleize <matias.leize@hotmail.com>
2020-03-16 23:36:29 -0300

    arreglo los paths

--- Software/Estadística/MCMC/pc_path.py
+++ Software/Estadística/MCMC/pc_path.py
@@ -1,9 +1,9 @@
 import socket
 def definir_path(): 
     if socket.gethostname() == 'matias-Satellite-A665':
-        path_git = '/home/matias/Documents/Tesis/tesis_licenciatura'
-        path_datos_global = '/home/matias/Documents/Tesis'
+        path_git = '/home/Documents/Tesis/tesis_licenciatura'
+        path_datos_global = '/home/Documents/Tesis'
     else: #socket.gethostname() == 'compu de Susana':
-        path_git = 'home/mleize/tesis_licenciatura'
-        path_datos_global = 'home/mleize'
+        path_git = '/home/mleize/tesis_licenciatura'
+        path_datos_global = '/home/mleize'
     return path_git, path_datos_global
