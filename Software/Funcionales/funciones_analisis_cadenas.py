b922fc7a64ce3d3a755178f483307a0da24dbf5f
matiasleize <matias.leize@hotmail.com>
2020-03-19 20:06:31 -0300

    Archivo con las funciones de analisis de las cadenas

--- Software/Funcionales/funciones_analisis_cadenas.py
+++ Software/Funcionales/funciones_analisis_cadenas.py
@@ -7,40 +7,52 @@ Created on Sun Feb  2 13:28:48 2020
 import numpy as np
 from matplotlib import pyplot as plt
 import time
+import emcee
+import corner
 
 
-def graficar_taus_vs_n(sampler):
-'''Grafico de la autocorrelación en función del largo de la cadena. (Hacer)'''
-	n = 100 * np.arange(1, index + 1)
-	y = autocorr[:index] #Aun no esta definido autocorr, calcularlo a partir de sampler..
-	plt.plot(n, n / 100.0, "--k")
-	plt.plot(n, y)
-	plt.xlim(0, n.max())
-	plt.ylim(0, y.max() + 0.1 * (y.max() - y.min()))
-	plt.xlabel("number of steps")
-	plt.ylabel(r"mean $\hat{\tau}$");
+def graficar_taus_vs_n(sampler, num_param=0):
+	'''Esta función grafica el tiempo de autocorrelación integraodo
+	 en función del largo de la cadena.'''
+	chain = sampler.get_chain()[:, :, num_param].T
+	#chain.shape[1] es el largo de las cadenas
 
-#%matplotlib qt5
-#plt.close()
+	# Compute the estimators for a few different chain lengths
+	N = np.exp(np.linspace(np.log(100), np.log(chain.shape[1]), 100)).astype(int)
+
+	#len_chain,nwalkers,ndim=sampler.get_chain().shape
+	#N = np.linspace(1, len_chain, 100).astype(int)
+	taus = np.empty(len(N))
+	for i, n in enumerate(N):
+	    taus[i] = emcee.autocorr.integrated_time(chain[:, :n],quiet=True);
+	taus=np.cumsum(taus)
+	plt.loglog(N, taus, '.-')
+	plt.plot(N, N / 100.0, "--k", label=r"$\tau = N/100$")
+
+	ylim = plt.gca().get_ylim()
+	plt.ylim(ylim)
+	plt.xlabel("number of samples, $N$")
+	plt.ylabel(r"$\tau$ estimates")
+	plt.legend(fontsize=14);
+
 def graficar_cadenas(sampler,labels= ['omega_m','b']):
-	'''Grafico de las cadenas'''
-	fig, axes = plt.subplots(2, figsize=(10, 7), sharex=True)
+	'''Esta función grafica las cadenas en función del largo de las mismas.'''
 	samples = sampler.get_chain()
+	len_chain,nwalkers,ndim=sampler.get_chain().shape
+	fig, axes = plt.subplots(ndim, figsize=(10, 7), sharex=True)
 	for i in range(ndim):
 	    ax = axes[i]
 	    ax.plot(samples[:, :, i], "k", alpha=0.3)
 	    ax.set_xlim(0, len(samples))
 	    ax.set_ylabel(labels[i])
-	    ax.yaxis.set_label_coords(-0.1, 0.5)
-
+	ax.yaxis.set_label_coords(-0.1, 0.5)
 	axes[-1].set_xlabel("step number");
 
-def grafiar_contornos(sampler,params_truths=[omega_m_ml,b_ml],discard=20,thin=None,labels= ['omega_m','b']):
-	'''Grafico de los contornos de confianza.'''
+def graficar_contornos(sampler,params_truths,discard=20,thin=None,labels= ['omega_m','b']):
+	'''Grafica los contornos de confianza.'''
 	if thin == None:
-		flat_samples = sampler.get_chain(discard=20, flat=True)
+		flat_samples = sampler.get_chain(discard, flat=True)
 	else:
-		flat_samples = sampler.get_chain(discard=20, flat=True, thin=thin)
+		flat_samples = sampler.get_chain(discard, flat=True, thin=thin)
 	print(flat_samples.shape)
 	fig = corner.corner(flat_samples, labels=labels, truths=params_truths);
-
