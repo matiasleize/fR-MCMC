b4a18cc171da06772cd386f44e52d5768dcdf0bc
matiasleize <matias.leize@hotmail.com>
2020-03-20 20:37:56 -0300

    Actualizo los pc_path

--- Software/Estadística/MCMC/pc_path.py
+++ Software/Estadística/MCMC/pc_path.py
@@ -3,7 +3,10 @@ def definir_path():
     if socket.gethostname() == 'matias-Inspiron-5570':
         path_git = '/home/matias/Documents/Tesis/tesis_licenciatura'
         path_datos_global = '/home/matias/Documents/Tesis'
-    else: #socket.gethostname() == 'compu de Susana':
+    elif: socket.gethostname() == 'quipus': #Compu de Susana
         path_git = '/home/mleize/tesis_licenciatura'
         path_datos_global = '/home/mleize'
+    else: 
+	path_git = '/home/mleize/tesis_licenciatura'
+	path_datos_global = '/home/mleize'
     return path_git, path_datos_global
