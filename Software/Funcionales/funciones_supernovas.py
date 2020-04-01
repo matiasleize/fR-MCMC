9629fa718af6e4f2a9fcc23638333ba99f9e0908
matiasleize <matias.leize@hotmail.com>
2020-04-01 03:09:07 -0300

    Actualizo este script.

--- Software/Funcionales/funciones_supernovas.py
+++ Software/Funcionales/funciones_supernovas.py
@@ -21,7 +21,6 @@ from funciones_cambio_parametros import params_fisicos_to_modelo
 
 #ORDEN DE PRESENTACION DE LOS PARAMETROS: Mabs,omega_m,b,H_0,n
 
-
 def chi_2_supernovas(muth,magn_aparente_obs,M_abs,C_invertida):
     '''Dado el resultado teórico muth y los datos de la
     magnitud aparente y absoluta observada, con su matriz de correlación
@@ -34,9 +33,63 @@ def chi_2_supernovas(muth,magn_aparente_obs,M_abs,C_invertida):
     chi2 = np.dot(transp,aux)/sn
     return chi2
 
+def params_to_chi2(cond_iniciales, theta, params_fijos, zcmb, zhel, Cinv,
+                    mb,cantidad_zs=3000, max_step=0.1, verbose=True):
+    '''Dados los parámetros del modelo devuelve un chi2 para los datos
+    de supernovas. 1 parámetro fijo y 4 variables'''
 
+    [Mabs,omega_m,b,H_0] = theta
+    n = params_fijos
 
-def params_to_chi2_M_H0_fijo(cond_iniciales, theta, params_fijos, zcmb, zhel, Cinv, mb):
+    ## Transformo los parametros fisicos en los del modelo:
+    c1,c2,r_hs = params_fisicos_to_modelo(omega_m,b,H_0,n)
+    params_modelo = [c1,r_hs/(H_0**2),c2,n] #de la cruz: [b,c,d,n]
+
+    z,E = integrador(cond_iniciales, params_modelo,
+                    cantidad_zs=cantidad_zs, max_step=max_step,verbose=verbose)
+    muth = magn_aparente_teorica(z,E,zhel,zcmb,H_0)
+    chi = chi_2_supernovas(muth,mb,Mabs,Cinv)
+    return chi
+
+def params_to_chi2_omega_H0_fijo(cond_iniciales, theta, params_fijos, zcmb,
+                                zhel, Cinv, mb, cantidad_zs=3000, max_step=0.1,
+                                verbose=True):
+    '''Dados los parámetros del modelo devuelve un chi2 para los datos
+    de supernovas. 1 parámetro fijo y 4 variables'''
+
+    [Mabs,b] = theta
+    [omega_m,H_0,n] = params_fijos
+
+    ## Transformo los parametros fisicos en los del modelo:
+    c1,c2,r_hs = params_fisicos_to_modelo(omega_m,b,H_0,n)
+    params_modelo = [c1,r_hs/(H_0**2),c2,n] #de la cruz: [b,c,d,n]
+
+    z,E = integrador(cond_iniciales, params_modelo,
+                    cantidad_zs=cantidad_zs, max_step=max_step,verbose=verbose)
+    muth = magn_aparente_teorica(z,E,zhel,zcmb,H_0)
+    chi = chi_2_supernovas(muth,mb,Mabs,Cinv)
+    return chi
+
+def params_to_chi2_H0_fijo(cond_iniciales, theta, params_fijos, zcmb, zhel,
+                            Cinv, mb, cantidad_zs=3000, max_step=0.1, verbose=True):
+    '''Dados los parámetros del modelo devuelve un chi2 para los datos de
+    supernovas. 2 parámetros fijos y 3 variables'''
+
+    [Mabs,omega_m,b] = theta
+    [H_0,n] = params_fijos
+
+    ## Transformo los parametros fisicos en los del modelo:
+    c1,c2,r_hs = params_fisicos_to_modelo(omega_m,b)
+    params_modelo = [c1,r_hs/(H_0**2),c2,n] #de la cruz: [b,c,d,n]
+
+    z,E = integrador(cond_iniciales, params_modelo,
+                    cantidad_zs=cantidad_zs, max_step=max_step,verbose=verbose)
+    muth = magn_aparente_teorica(z,E,zhel,zcmb,H_0)
+    chi = chi_2_supernovas(muth,mb,Mabs,Cinv)
+    return chi
+
+def params_to_chi2_M_H0_fijo(cond_iniciales, theta, params_fijos, zcmb, zhel,
+                            Cinv, mb, cantidad_zs=3000, max_step=0.1, verbose=True):
     '''Dados los parámetros del modelo devuelve un chi2 para los datos de
     supernovas. 3 parámetros fijos y 2 variables.'''
 
@@ -47,41 +100,8 @@ def params_to_chi2_M_H0_fijo(cond_iniciales, theta, params_fijos, zcmb, zhel, Ci
     c1,c2,r_hs = params_fisicos_to_modelo(omega_m,b)
     params_modelo = [c1,r_hs/(H_0**2),c2,n] #de la cruz: [b,c,d,r_0,n]
 
-    z,E = integrador(cond_iniciales, params_modelo, cantidad_zs=2000, max_step=0.05)
-    muth = magn_aparente_teorica(z,E,zhel,zcmb)
-    chi = chi_2_supernovas(muth,mb,Mabs,Cinv)
-    return chi
-
-def params_to_chi2_H0_fijo(cond_iniciales, theta, params_fijos, zcmb, zhel, Cinv, mb):
-    '''Dados los parámetros del modelo devuelve un chi2 para los datos de
-    supernovas. 2 parámetros fijos y 3 variables'''
-
-    [Mabs,omega_m,b] = theta
-    [H_0,n] = params_fijos
-
-    ## Transformo los parametros fisicos en los del modelo:
-    c1,c2,r_hs = params_fisicos_to_modelo(omega_m,b)
-    params_modelo = [c1,r_hs,c2,n] #de la cruz: [b,c(*H0^2),d,r_0,n]
-
-    z,E = integrador(cond_iniciales, params_modelo)
-    muth = magn_aparente_teorica(z,E,zhel,zcmb)
-    chi = chi_2_supernovas(muth,mb,Mabs,Cinv)
-    return chi
-
-
-def params_to_chi2(cond_iniciales, theta, params_fijos, zcmb, zhel, Cinv, mb,verbose=True):
-    '''Dados los parámetros del modelo devuelve un chi2 para los datos
-    de supernovas. 1 parámetro fijo y 4 variables'''
-
-    [Mabs,omega_m,b,H_0] = theta
-    n = params_fijos
-
-    ## Transformo los parametros fisicos en los del modelo:
-    c1,c2,r_hs = params_fisicos_to_modelo(omega_m,b,H_0)
-    params_modelo = [c1,r_hs,c2,n] #de la cruz: [b,c(*H0^2),d,r_0,n]
-
     z,E = integrador(cond_iniciales, params_modelo,
-                    cantidad_zs=2000, max_step=0.05,verbose=verbose)
-    muth = magn_aparente_teorica(z,E,zhel,zcmb)
+                    cantidad_zs=cantidad_zs, max_step=max_step,verbose=verbose)
+    muth = magn_aparente_teorica(z,E,zhel,zcmb,H_0)
     chi = chi_2_supernovas(muth,mb,Mabs,Cinv)
     return chi
