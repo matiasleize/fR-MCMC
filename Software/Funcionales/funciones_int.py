e28af9d3c5f94c6bd654a2721270b3fc15a13d4d
matiasleize <matias.leize@hotmail.com>
2020-09-29 12:44:28 -0300

    Actualizo la función de integración numérica, calculando las CI para cada set de parámetros e integrando de z=30 a z=0.

--- Software/Funcionales/funciones_int.py
+++ Software/Funcionales/funciones_int.py
@@ -4,15 +4,22 @@ Created on Sun Feb  2 13:28:48 2020
 @author: matias
 """
 
+import time
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.integrate import solve_ivp
-from scipy.integrate import simps as simps
-from scipy.integrate import cumtrapz as cumtrapz
-import time
-
-from scipy.interpolate import interp1d
 from scipy.constants import c as c_luz #metros/segundos
+c_luz_norm = c_luz/1000;
+
+import sys
+import os
+from os.path import join as osjoin
+from pc_path import definir_path
+path_git, path_datos_global = definir_path()
+os.chdir(path_git)
+sys.path.append('./Software/Funcionales/')
+from funciones_condiciones_iniciales import condiciones_iniciales
+from funciones_cambio_parametros import params_fisicos_to_modelo
 
 def dX_dz(z, variables,*params_modelo, model='HS'):
     '''Defino el sistema de ecuaciones a resolver. El argumento params_modelo
@@ -26,17 +33,8 @@ def dX_dz(z, variables,*params_modelo, model='HS'):
     w = variables[3]
     r = variables[4]
 
-    #nombre_modelo = params_modelo[-1]
-    #if nombre_modelo == 'Star':
-#        gamma = lambda r,b,c,d,n : b*r
-#        [B,C,D,N,_] = params_modelo
-#        G = gamma(r,B,C,D,N)
-#    elif nombre_modelo == 'HS':
-        #if params_modelo[3]==1:
-        #    gamma = lambda y,v: y*v/(2*(y-v)**2)
-        #    G = gamma(y,v)
-        #else:
-            #[b,d,c,n] = parametros_modelo
+    if model == 'ST':
+        pass
     if model == 'HS':
         [B,D,N] = params_modelo
         if N==1:
@@ -52,13 +50,12 @@ def dX_dz(z, variables,*params_modelo, model='HS'):
     s1 = (- (v*x*G - x*y + 4*y - 2*y*v)) / (z+1)
     s2 = (-v * (x*G + 4 - 2*v)) / (z+1)
     s3 = (w * (-1 + x + 2*v)) / (z+1)
-    s4 = (-x * r * G) / (1+z)
+    s4 = (-(x * r * G)) / (1+z)
     return [s0,s1,s2,s3,s4]
 
 
-def integrador(cond_iniciales, params_modelo, cantidad_zs, max_step,
-                sistema_ec=dX_dz, z_inicial=0, z_final=3, verbose=False):
-                #cantidad_zs_ideal = 7000, # max_step_ideal = 0.005
+def integrador(params_fisicos, n=1, cantidad_zs=int(10**6), max_step=0.01,
+                z_inicial=30, z_final=0, sistema_ec=dX_dz, verbose=False):
 
     '''Esta función integra el sistema de ecuaciones diferenciales entre
     z_inicial y z_final, dadas las condiciones iniciales y los parámetros
@@ -69,33 +66,38 @@ def integrador(cond_iniciales, params_modelo, cantidad_zs, max_step,
     Para supernovas:  max_step=0.05
     '''
     t1 = time.time()
-    # Integramos el vector v y calculamos el Hubble
-    zs = np.linspace(z_inicial,z_final,cantidad_zs)
+
+    [omega_m,b,H0] = params_fisicos
+
+    #Calculo las condiciones cond_iniciales, eta
+    # y los parametros de la ecuación
+    cond_iniciales = condiciones_iniciales(*params_fisicos)
+    eta = (c_luz_norm/(8315*100)) * np.sqrt(omega_m/0.13)
+    c1, c2 = params_fisicos_to_modelo(omega_m,b)
+
+    #Integramos el sistema
+    zs_int = np.linspace(z_inicial,z_final,cantidad_zs)
     sol = solve_ivp(sistema_ec, (z_inicial,z_final),
-    cond_iniciales, t_eval=zs, args=params_modelo, max_step=max_step)
+        cond_iniciales, t_eval=zs_int, args=[c1,c2,n], max_step=max_step)
 
     if (len(sol.t)!=cantidad_zs):
         print('Está integrando mal!')
-    if np.all(zs==sol.t)==False:
+    if np.all(zs_int==sol.t)==False:
         print('Hay algo raro!')
 
-    #Chequear este paso!
-    #hubbles = np.ones(len(sol.t)) #Para que el primer valor de este array sea 1!
-    #for i in range (1, len(zs)):
-    #    int_v =  simps((sol.y[2][:i])/(1+sol.t[:i]),sol.t[:i])
-    #    hubbles[i] = (1+sol.t[i])**2 * np.e**(-int_v)
-
-    int_v =  cumtrapz(sol.y[2]/(1+sol.t), sol.t, initial=0)
-    hubbles = (1+sol.t)**2 * np.exp(-int_v)
+    #Calculamos el Hubble
+    zs = sol.t[::-1]
+    v=sol.y[2][::-1]
+    r=sol.y[4][::-1]
+    Hs = H0 * (eta * np.sqrt(r/(6*v)))
 
     t2 = time.time()
+
     if verbose == True:
-        pass
-#        print('Duración {} minutos y {} segundos'.format(int((t2-t1)/60),
-#        int((t2-t1) - 60*int((t2-t1)/60))))
+        print('Duración {} minutos y {} segundos'.format(int((t2-t1)/60),
+              int((t2-t1) - 60*int((t2-t1)/60))))
 
-    return sol.t, hubbles
-
+    return zs, Hs
 
 def plot_sol(solucion):
 
@@ -116,35 +118,36 @@ def plot_sol(solucion):
 #%%
 if __name__ == '__main__':
 
-    sistema_ec=dX_dz
-    z_inicial = 0
-    z_final = 3
-    cantidad_zs = 1000000
-    max_step = 1
-    verbose = True
-
-
-    x_0 = -0.339
-    y_0 = 1.246
-    v_0 = 1.64
-    w_0 = 1 + x_0 + y_0 - v_0
-    r_0 = 41
-    ci = [x_0, y_0, v_0, w_0, r_0] #Condiciones iniciales
-    cond_iniciales = ci
-#%%
-    #c1_true = 1
-    #c2_true = 1/19
-    #n=1
-    #Parametros problematicos
-    #params_modelo=[400.34475698232389, 100.48051181565308,1]
-    #params_modelo=[0.075, -0.25,1]
-    params_modelo = [1,1/19,1]
-
-#%% Forma nueva de integrar
-    #%matplotlib qt5
     from matplotlib import pyplot as plt
+    import numpy as np
+    from scipy.integrate import cumtrapz as cumtrapz
 
-    plt.figure()
+    import sys
+    import os
+    from os.path import join as osjoin
+    from pc_path import definir_path
+    path_git, path_datos_global = definir_path()
+    os.chdir(path_git)
+    sys.path.append('./Software/Funcionales/')
+    from funciones_taylor import Taylor_HS
+
+    sistema_ec=dX_dz
+    z_inicial = 30
+    z_final = 0
+    cantidad_zs = int(10**6)
+    max_step = 0.01
+
+    omega_m = 0.24
+    b = 2
+    H0 = 73.48
+    params_fisicos = [omega_m,b,H0]
+
+    cond_iniciales= condiciones_iniciales(*params_fisicos)
+    eta = (c_luz_norm/(8315*100)) * np.sqrt(omega_m/0.13)
+    c1,c2 = params_fisicos_to_modelo(omega_m,b)
+    params_modelo=[c1,c2,1]
+
+    #%% Grafico lo que sale de la ODE
     zs = np.linspace(z_inicial,z_final,cantidad_zs)
     t1 = time.time()
     sol = solve_ivp(sistema_ec, [z_inicial,z_final],
@@ -152,35 +155,71 @@ if __name__ == '__main__':
     t2 = time.time()
     print('Duración {} minutos y {} segundos'.format(int((t2-t1)/60),
           int((t2-t1) - 60*int((t2-t1)/60))))
+
+    plt.figure()
     plot_sol(sol)
-    np.all(zs==sol.t)
-    print(max(sol.t))
-    print(max(zs))
+    print(np.all(zs==sol.t))
+    #%%
+    def H_LCDM(z, omega_m, H_0):
+        omega_lambda = 1 - omega_m
+        H = H_0 * np.sqrt(omega_m * (1 + z)**3 + omega_lambda)
+        return H
 
-#%%
-    plt.figure()
-    #E=np.ones(len(sol. t))
-    #for i in range (1, len(sol.t)):
-    #    int_v =  simps((sol.y[2][:i])/(1+sol.t[:i]),sol.t[:i])
-    #    E[i] = (1+sol.t[i])**2 * np.e**(-int_v)
-    int_v =  cumtrapz((sol.y[2])/(1+sol.t),sol.t,initial=0)
-    E = (1+sol.t)**2 * np.exp(-int_v)
-    plt.plot(sol.t,E)
-#%% Forma vieja de integrar (no integra bien, pero oculta el error!)
-    plt.figure()
-    t1 = time.time()
-    zs = np.linspace(z_inicial,z_final,cantidad_zs)
-    hubbles_1 = np.zeros(len(zs))
-    for i in range(len(zs)):
-        zf = zs[i] # ''z final'' para cada paso de integracion
-        sol_1 = solve_ivp(sistema_ec,[z_inicial,zf], cond_iniciales,args=params_modelo, max_step=max_step)
-        int_v = simps((sol_1.y[2])/(1+sol_1.t),sol_1.t) # integro desde 0 a z
-        hubbles_1[i] = (1+zf)**2 * np.exp(-int_v)
-        np.all(zs==sol_1.t)
-    t2 = time.time()
-    print('Duración {} minutos y {} segundos'.format(int((t2-t1)/60),
-          int((t2-t1) - 60*int((t2-t1)/60))))
-    plt.plot(zs,hubbles_1)
+    r=sol.y[4][::-1]
+    v=sol.y[2][::-1]
+    z_int=sol.t[::-1]
 
-    len(sol_1.t)
-    len(zs)
+    E_LCDM = H_LCDM(z_int,omega_m,1)
+    E_ODE = eta*np.sqrt(r/(6*v))
+    #E_taylor = Taylor_HS(z_int,omega_m,b,H0)/H0
+
+
+    #%% Ploteo y comparo las soluciones
+    #%matplotlib qt5
+    plt.close()
+    plt.figure()
+    plt.grid(True)
+    plt.xlabel('z (redshift)')
+    plt.ylabel('H(z)')
+    plt.plot(z_int,E_ODE,label='ODE')
+    #plt.plot(z_int,E_taylor,'-.',label='Taylor')
+    plt.plot(z_int,E_LCDM,'-.',label='LCDM')
+    plt.legend(loc='best')
+    plt.xlim([0,3])
+    plt.ylim([0,6])
+
+    #%% Calculo el error porcentual entre la ODE y el taylor a b fijo.
+    plt.figure()
+    plt.grid(True)
+    plt.xlabel('z (redshift)')
+    plt.ylabel('Error porcentual')
+    plt.plot(z_int,100*(1-(E_taylor/E_ODE)),label='Error porcentual')
+    plt.legend(loc='best')
+    plt.show()
+    np.mean(100*(1-(E_taylor/E_ODE)))
+
+    #%% Da bien con un error de 10(-12)
+    plt.plot(sol.t,sol.y[3]+sol.y[2]-sol.y[0]-sol.y[1])
+
+    #%% Ricci(z) para HS
+    r_inv=sol.y[4][::-1]
+    zs=np.linspace(0,10,len(r_inv))
+    plt.grid(True)
+    plt.xlabel('z (redshift)')
+    plt.ylabel(r'$R_{HS}/R$')
+    plt.plot(zs,r_inv)
+    plt.show()
+
+    #%% Esto es lo viejo que no anda!
+    plt.figure()
+    z_int=sol.t[::-1]
+    v_int=sol.y[2][::-1]
+    int_v =  cumtrapz(v_int/(1+z_int),z_int,initial=0)
+    E = (1+z_int)**2 * np.exp(-int_v)
+    plt.figure()
+    plt.plot(z_int,E_LCDM,'.')
+    plt.plot(z_int,E)
+    #%% Error en la integración de v de 10(-3) (Tambien Viejo)
+    dx=z_int[1]-z_int[0]
+    dH = np.diff(E)/dx
+    plt.plot(100*(1-((2-v_int)*E/(1+z_int))[1:]/dH))
