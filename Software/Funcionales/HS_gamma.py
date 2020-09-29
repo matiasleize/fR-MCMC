dca09bfb6e3462d53f0dc732669865fa6ccf3960
matiasleize <matias.leize@hotmail.com>
2020-09-29 12:48:45 -0300

    Actualizo el cálculo de la gamma de HS para la ODE.

--- Software/Funcionales/HS_gamma.py
+++ Software/Funcionales/HS_gamma.py
@@ -27,13 +27,16 @@ F = R - ((c1*R)/((c2*R/(c*H0**2))+1))
 F_R = sym.simplify(sym.diff(F,R))
 F_2R = sym.simplify(sym.diff(F_R,R))
 
-print(F_R)
+gamma_2 = F_R/(R*F_2R)
+print(sym.simplify(gamma_2))
 
-gamma = F_R/(R*F_2R)
-print(sym.simplify(gamma))
+gamma_1=((R*F_R/F)-1)**(-2)
+print(sym.simplify(gamma_1/gamma_2))
 
+aux=-(F_R**2/F*F_2R)
+print(sym.simplify(aux))
 
-
+#%%
 #func = lambdify(N, HS_tay,'numpy') # returns a numpy-ready function
 
 #    N_dato = -np.log(1+z)
@@ -44,4 +47,33 @@ R = r*(c*H0**2)
 
 #%%
 if __name__ == '__main__':
+    import sys
+    from numpy import linspace
+    from scipy.integrate import odeint
+    from scipy.optimize import fsolve
+    from matplotlib import pyplot as plt
+
+    y0 = [0, 5] #condiciones iniciales
+    time = linspace(0., 10., 1000)
+    #parametros
+    F_lon = 10.
+    mass = 1000.
+
+    def Ricci(H, H_N):
+        return 6*(2H**2+H+H_N)
+    def H_lambda(N):
+        return np.sqrt((1+N))
+    def constraint(a, v):
+        return (F_lon - F_r(a, v)) / mass - a
+
+    def integral(y, _):
+        v = y[1]
+        a, _, ier, mesg = fsolve(constraint, 0, args=[v, ], full_output=True)
+        if ier != 1:
+            print ("I coudn't solve the algebraic constraint, error:\n\n", mesg)
+            sys.stdout.flush()
+        return [v, a]
+
+    dydt = odeint(integral, y0, time)
+    plt.plot(dydt[1])
     pass
