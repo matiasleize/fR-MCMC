12f0e85c0cb1bddac8ab5a3ead591d9af7934c91
matiasleize <matias.leize@hotmail.com>
2020-10-10 15:46:15 -0300

    Elimino params_to_chi2_taylor y lo junto todo en la misma función params_to_chi2.

--- Software/Funcionales/funciones_cronometros.py
+++ Software/Funcionales/funciones_cronometros.py
@@ -31,7 +31,7 @@ def chi_2_cronometros(H_teo,H_data, dH):
 
 def params_to_chi2(theta, params_fijos, z_data, H_data, dH,
                     cantidad_zs=10000, max_step=0.005,
-                    verbose=True, model='HS', chi_riess=True):
+                    verbose=True, model='HS', chi_riess=True, taylor=False):
     '''Dados los parámetros libres del modelo (omega, b y H0) y los que quedan params_fijos (n),
     devuelve un chi2 para los datos de los
     cronómetros cósmicos'''
@@ -44,60 +44,26 @@ def params_to_chi2(theta, params_fijos, z_data, H_data, dH,
         [H_0,n] = params_fijos
         chi_H0 = 0
 
-    if (0 <= b < 0.1):
+    if taylor == True:
         if model=='HS':
             H_teo = Taylor_HS(z_data, omega_m, b, H_0)
         else:
             H_teo = Taylor_ST(z_data, omega_m, b, H_0)
 
     else:
-        params_fisicos = [omega_m,b,H_0]
-        z, H = integrador(params_fisicos, n)
-        H_int = interp1d(z,H)
-        H_teo = H_int(z_data)
-
-    chi = chi_2_cronometros(H_teo,H_data,dH)
-
-
-    if chi_riess==True:
-        return chi+chi_H0
-    else:
-        return chi
-
-
-def params_to_chi2_taylor(theta, params_fijos, z_data, H_data, dH,
-                            omega_fijo=False,model='HS',chi_riess=True,
-                            nunes=False):
-    '''Dados los parámetros libres del modelo (omega, b y H0) y los que quedan params_fijos (n),
-    devuelve un chi2 para los datos de cronómetros cósmicos'''
-
-
-    if len(theta)==3:
-        [omega_m,b,H_0] = theta
-        n = params_fijos
-        if nunes==True:
-            chi_H0 = ((H_0-73.02)/1.79)**2 #El que usa Nunes
-        else:
-            chi2_H0 = ((H_0-73.48)/1.66)**2
-    elif len(theta)==2:
-        if omega_fijo==True:
-            [b,H_0] = theta
-            [omega_m,n] = params_fijos
-            if nunes==True:
-                chi_H0 = ((H_0-73.02)/1.79)**2 #El que usa Nunes
+        if (0 <= b < 0.1):
+            if model=='HS':
+                H_teo = Taylor_HS(z_data, omega_m, b, H_0)
             else:
-                chi2_H0 = ((H_0-73.48)/1.66)**2
+                H_teo = Taylor_ST(z_data, omega_m, b, H_0)
         else:
-            [omega_m,b] = theta
-            [H_0,n] = params_fijos
-            chi_H0 = 0
-
-    if model=='HS':
-        H_teo = Taylor_HS(z_data, omega_m, b, H_0)
-    else:
-        H_teo = Taylor_ST(z_data, omega_m, b, H_0)
+            params_fisicos = [omega_m,b,H_0]
+            z, H = integrador(params_fisicos, n)
+            H_int = interp1d(z,H)
+            H_teo = H_int(z_data)
 
     chi = chi_2_cronometros(H_teo,H_data,dH)
+
 
     if chi_riess==True:
         return chi+chi_H0
