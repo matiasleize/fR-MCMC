d4295ae06d3beccfe9d82bd701e6466233da0f44
matiasleize <matias.leize@hotmail.com>
2020-10-12 00:26:05 -0300

    Hago barrido de parametros para omega entre .3 y .4

--- Software/Sandbox/Integracion numérica/tuneo_parametros/int_mapa_parametros.py
+++ Software/Sandbox/Integracion numérica/tuneo_parametros/int_mapa_parametros.py
@@ -13,7 +13,7 @@ from matplotlib import pyplot as plt
 from scipy.integrate import solve_ivp
 import time
 from scipy.constants import c as c_luz #metros/segundos
-c_luz_norm = c_luz/1000
+c_luz_km = c_luz/1000
 
 import sys
 import os
@@ -29,29 +29,23 @@ from funciones_int import dX_dz
 
 #%%
 sistema_ec=dX_dz
-z_inicial = 0
-z_final = 3
-cantidad_zs = 1000000
-max_step = np.inf #0.01
-verbose = True
-H0=73.48
+z_inicial = 30
+z_final = 0
+cantidad_zs = int(10**5)
+max_step = 0.003
 n=1
 
 zs = np.linspace(z_inicial,z_final,cantidad_zs)
 
-omegas = np.linspace(0.01,0.8,10)
-bs = np.linspace(-0.01,1.5,10)
-
+omegas = np.linspace(0.3,0.35,20)
+bs = np.linspace(0.1,0.16,50)
 errores = np.zeros((len(omegas),len(bs)))
+t1 = time.time()
 for i, omega_m in enumerate(omegas):
     for j, b in enumerate(bs):
 
-        params_fisicos = [omega_m,b,H0]
-
-        #Calculo las condiciones cond_iniciales, eta
-        # y los parametros de la ecuación
-        cond_iniciales = condiciones_iniciales(*params_fisicos)
-        eta = (c_luz_norm/(8315*100)) * np.sqrt(omega_m/0.13)
+        cond_iniciales = condiciones_iniciales(omega_m,b)
+        eta = (c_luz_km/(8315*100)) * np.sqrt(omega_m/0.13)
         c1, c2 = params_fisicos_to_modelo(omega_m,b)
 
         #Integramos el sistema
@@ -60,16 +54,25 @@ for i, omega_m in enumerate(omegas):
             cond_iniciales, t_eval=zs_int, args=[c1,c2,n], max_step=max_step)
 
         #if (sol.success!=True): #Otra formaa
-        if (len(sol.t)!=cantidad_zs):
-            errores[i,j] = 1
-        if np.all(zs==sol.t)==False:
-            errores[i,j] = 2
 
+        if (len(sol.t)!=cantidad_zs):
+            errores[i,j] = 3 #Esto es lo peor
+        elif np.all(zs==sol.t)==False:
+            errores[i,j] = 2 #Esto es menos peor
+        else:
+            errores[i,j] = 1 #Esto es correcto :)
+        #dos y tres son errores, uno es todo piola
+t2 = time.time()
+print('Duración {} minutos y {} segundos'.format(int((t2-t1)/60),
+      int((t2-t1) - 60*int((t2-t1)/60))))
+
+om_malos = omegas[np.where(errores==3)[0]]
+bs_malos = bs[np.where(errores==3)[1]]
+print(om_malos)
+print(bs_malos)
+
+#%%
 %matplotlib qt5
-#np.where(errores==1)
-om_malos = omegas[np.where(errores==1)[0]]
-bs_malos = bs[np.where(errores==1)[1]]
-#print(bs_malos)
 plt.close()
 plt.figure(1)
 plt.xlabel('b')
@@ -79,3 +82,26 @@ plt.figure(2)
 plt.matshow(errores)
 #plt.colorbar()
 plt.show()
+errores
+
+
+#Errores que aparecieron en la integración para SN:
+#[0.35555556, 0.11034483]
+#[0.36666667, 0.15517241]
+#[0.35263158, 0.10461538]
+#[0.33351188   0.11611847]*
+#[0.36000551   0.10510931]*
+#(*) errores que aparecieron con los nuicense aprameters de SN
+
+#Barrido exhautivo para  entre 0.1 y 0.2 y omega entre 0.3 y 0.4:
+#[0.34210526 0.35789474 0.36315789 0.36842105 0.38421053 0.38947368
+# 0.39473684 0.39473684 0.39473684 0.39473684 0.39473684]
+#[0.10122449 0.10653061 0.10285714 0.1155102  0.10489796 0.10285714
+# 0.1044898  0.10530612 0.10571429 0.1077551  0.10897959]
+
+#[0.35263158 0.35894737 0.36210526 0.37157895 0.37473684 0.37473684
+# 0.37473684 0.37789474 0.38105263 0.38421053 0.39052632 0.39052632
+# 0.39052632 0.39368421 0.39684211 0.39684211 0.4       ]
+#[0.1        0.10122449 0.1        0.13918367 0.10489796 0.10612245
+# 0.15632653 0.12938776 0.10857143 0.10489796 0.10489796 0.12571429
+# 0.13795918 0.13306122 0.10489796 0.13183673 0.10734694]
