642029d9a5f0015039e74400da9b7c645c77d668
matiasleize <matias.leize@hotmail.com>
2020-10-23 21:53:38 -0300

    Agrego el analisis para HS n=2 y St n=1.

--- Software/Funcionales/funciones_int.py
+++ Software/Funcionales/funciones_int.py
@@ -13,7 +13,7 @@ os.chdir(path_git)
 sys.path.append('./Software/Funcionales/')
 
 from funciones_condiciones_iniciales import condiciones_iniciales
-from funciones_cambio_parametros import params_fisicos_to_modelo_HS
+from funciones_cambio_parametros import params_fisicos_to_modelo_HS, params_fisicos_to_modelo_ST
 #%%
 
 def dX_dz(z, variables, params_modelo, model='HS'):
@@ -30,25 +30,33 @@ def dX_dz(z, variables, params_modelo, model='HS'):
 
     #[p1,p2,n,model]=imput
 
-    if model == 'ST':
+    if model == 'ST': #Para el modelo de Starobinsky
         [lamb,N] = params_modelo
         if N==1:
-            gamma = lambda r,lamb: (r**2+1) * ((r**2+1)**2-2*r*lamb)/(2*r*lamb*(3*r**2-1))
-            G = gamma(r,lamb)
+            gamma = lambda r,lamb: -(r**2 + 1)*(2*lamb*r - (r**2 + 1)**2)/(2*lamb*r*(3*r**2 - 1))
+            G = gamma(r,lamb) #Va como r^6/r^3 = r^3
         else:
+            (r**2 + 1)**(n + 2)*(-lamb*n*r*(r**2 + 1)**(-n - 1) + 1/2)/(lamb*n*r*(2*n*r**2 + r**2 - 1))
             print('Guarda que estas poniendo n!=1')
             pass
-    elif model == 'HS':
+
+
+    elif model == 'HS': #Para el modelo de Hu-Sawicki
         [B,D,N] = params_modelo
         if N==1:
-            gamma = lambda r,b,d: ((1+d*r) * (-b*r + r*(1+d*r)**2)) / (b*2*d*r**2)
-            G = gamma(r,B,D)
+            gamma = lambda r,c1,c2: -(c1 - (c2*r + 1)**2)*(c2*r + 1)/(2*c1*c2*r)
+            G = gamma(r,B,D) #Va como r^3/r = r^2
+        elif N==2:
+            gamma = lambda r,c1,c2: (-2*c1*c2*r**3 - 2*c1*r + c2**3*r**6 + 3*c2**2*r**4 + 3*c2*r**2 + 1)/(2*c1*r*(3*c2*r**2 - 1))
+            G = gamma(r,B,D) #Va como r^6/r^3 = r^3
         else:
-            gamma = lambda r,b,d,n: ((1+d*r**n) * (-b*n*r**n + r*(1+d*r**n)**2)) / (b*n*(1 - n + d * (1+n) * r**n)*r**n)
+            gamma = lambda r,c1,c2,n: r**(-n)*(-c1*c2*n*r**(2*n) - c1*n*r**n + c2**3*r**(3*n + 1) + 3*c2**2*r**(2*n + 1) + 3*c2*r**(n + 1) + r)/(c1*n*(c2*n*r**n + c2*r**n - n + 1))
             G = gamma(r,B,D,N)
-            print('Guarda que estas poniendo n!=1')
+            #print('Guarda que estas poniendo n!=1')
     else:
+        print('Elegir un modelo válido!')
         pass
+
     s0 = (-w + x**2 + (1+v)*x - 2*v + 4*y) / (z+1)
     s1 = (- (v*x*G - x*y + 4*y - 2*y*v)) / (z+1)
     s2 = (-v * (x*G + 4 - 2*v)) / (z+1)
@@ -75,18 +83,17 @@ def integrador(params_fisicos, n=1, cantidad_zs=int(10**5), max_step=0.003,
         [omega_m, b, H0] = params_fisicos
         #Calculo las condiciones cond_iniciales, eta
         # y los parametros de la ecuación
-        cond_iniciales = condiciones_iniciales(omega_m, b)
+        cond_iniciales = condiciones_iniciales(omega_m, b, z0=z_inicial, n=n)
         eta = (c_luz_km/(8315*100)) * np.sqrt(omega_m/0.13)
-        c1, c2 = params_fisicos_to_modelo_HS(omega_m, b)
+        c1, c2 = params_fisicos_to_modelo_HS(omega_m, b, n=n)
 
         params_modelo = [c1,c2,n]
-        cantidad_zs = int(10**5)
-        max_step = 0.003
 
     elif model=='ST':
         #OJO Rs depende de H0, pero no se usa :) No como HS!! Cambiarlo en la tesis!!!
         [omega_m, b, H0] = params_fisicos
         lamb = 2/b
+
         cond_iniciales = condiciones_iniciales(omega_m, b, model='ST')
         eta = np.sqrt(3 * (1 - omega_m) * b)
 
@@ -129,7 +136,7 @@ if __name__ == '__main__':
 
     z_inicial = 30
     z_final = 0
-    cantidad_zs = int(10**4)
+    cantidad_zs = int(10**5)
     max_step = 10**(-5)
 
     zs, H_ode = integrador(params_fisicos, n=1, cantidad_zs=cantidad_zs,
@@ -143,12 +150,10 @@ if __name__ == '__main__':
     plt.title('Parámetro de Hubble por integración numérica')
     plt.xlabel('z(redshift)')
     plt.ylabel('H(z)')
-    plt.plot(zs, H_ode, label='ODE')
+    plt.plot(zs, H_ode/H0, label='ODE')
     plt.legend(loc='best')
 
-    H_ode
-
-#%% Testeamos el cumtrapz comparado con simpson para la integral de 1/H
+    #%% Testeamos el cumtrapz comparado con simpson para la integral de 1/H
     from scipy.integrate import simps,trapz,cumtrapz
 
     def integrals(ys, xs):
@@ -173,4 +178,12 @@ if __name__ == '__main__':
     #plt.plot(zs,integral_1)
     plt.figure(2)
     plt.plot(zs,1-integral_1/integral) #Da un warning xq da 0/0 en z=0
-    
+
+#Para HS n=2 tarda 9 minutos y 30 seg con b = 0.7
+#Para HS n=2 tarda 9 minutos y 37 seg con b = 0.1
+#Para HS n=2 tarda 9 minutos y 39 seg con b = 0.08
+
+#Para ST n=1 tarda 8 minutos y 30 seg con b = 2
+#Para ST n=1 tarda 8 minutos y 22 seg con b = 1
+#Para ST n=1 tarda 8 minutos y 11 seg con b = 0.1
+#Para ST n=1 tarda 8 minutos y 17 seg con b = 0.08
--- Software/Sandbox/Integracion numérica/chequeos_ODE/comparacion_taylor_barrido_en_b.py
+++ Software/Sandbox/Integracion numérica/chequeos_ODE/comparacion_taylor_barrido_en_b.py
@@ -9,15 +9,77 @@ path_git, path_datos_global = definir_path()
 os.chdir(path_git)
 sys.path.append('./Software/Funcionales/')
 from funciones_int import integrador
-from funciones_taylor import Taylor_HS
+from funciones_taylor import Taylor_HS, Taylor_ST
 
 #%%
 bs=np.array([0.01, 0.02, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 1, 2])
+bs=np.array([0.08,0.1, 0.2, 0.3, 0.4, 0.5, 1, 2])
 #bs=np.linspace(0.01,2,100)
 error_b=np.zeros(len(bs))
 omega_m=0.24
 H0=73.48
 
+
+#%% Hu-Sawicki n=2
+for i, b in enumerate(bs):
+    print(i)
+    params_fisicos = [omega_m,b,H0]
+
+    zs, H_ode = integrador(params_fisicos, cantidad_zs=int(10**5), max_step=10**(-5),model='HS',n=2)
+    H_taylor = Taylor_ST(zs,omega_m,b,H0)
+
+    error_b[i] = 100*np.abs(np.mean(1-(H_taylor/H_ode)))
+#Tarda 1h y 20 aprox
+#%%
+%matplotlib qt5
+plt.close()
+plt.figure()
+plt.title('Error Porcentual para Hu-Sawicki n=2')
+plt.yscale('log')
+plt.grid(True)
+plt.xlabel('b')
+plt.ylabel('Error Porcentual')
+plt.plot(bs,error_b,label='Error en b')
+plt.legend(loc='best')
+plt.show()
+error_b
+
+#%%
+#b=0.5
+#params_fisicos = [omega_m,b,H0]
+#zs, H_ode = integrador(params_fisicos, cantidad_zs=int(10**3), max_step=60**(-5),model='ST',n=1)
+#H_taylor = Taylor_ST(zs,omega_m,b,H0)
+
+#plt.plot(zs,H_ode/H0)
+#plt.plot(zs,H_taylor/H0)
+#plt.plot(zs,H_ode/H_taylor)
+#%%
+
+#%% Starobinsky n=1
+for i, b in enumerate(bs):
+    print(i)
+    params_fisicos = [omega_m,b,H0]
+
+    zs, H_ode = integrador(params_fisicos, cantidad_zs=int(10**5), max_step=10**(-5),model='ST')
+    H_taylor = Taylor_ST(zs,omega_m,b,H0)
+
+    error_b[i] = 100*np.abs(np.mean(1-(H_taylor/H_ode)))
+#Tarda 1h y 15 aprox
+#%%
+%matplotlib qt5
+plt.close()
+plt.figure()
+plt.title('Error Porcentual para Starobinsky n=1')
+plt.yscale('log')
+plt.grid(True)
+plt.xlabel('b')
+plt.ylabel('Error Porcentual')
+plt.plot(bs,error_b,label='Error en b')
+plt.legend(loc='best')
+plt.show()
+error_b
+
+#%% Hu-Sawicki n=1
 for i, b in enumerate(bs):
     print(i)
     params_fisicos = [omega_m,b,H0]
@@ -26,11 +88,12 @@ for i, b in enumerate(bs):
     H_taylor = Taylor_HS(zs,omega_m,b,H0)
 
     error_b[i] = 100*np.abs(np.mean(1-(H_taylor/H_ode)))
-
+#Conclusion: Para HS n=1, max_step mayor a 0.001 lleva a resultados erroneos!.
 #%%
 %matplotlib qt5
 plt.close()
 plt.figure()
+plt.title('Error Porcentual para Hu-Sawicki n=1')
 plt.yscale('log')
 plt.grid(True)
 plt.xlabel('b')
@@ -38,5 +101,4 @@ plt.ylabel('Error Porcentual')
 plt.plot(bs,error_b,label='Error en b')
 plt.legend(loc='best')
 plt.show()
-
-#Conclusion: max_step mayor a 0.001 lleva a resultados erroneos!.
+error_b
