afecbb11144c4032c5c6d6e0f3be5b5394f1a8b8
matiasleize <matias.leize@hotmail.com>
2021-01-08 18:41:59 -0300

    Actualizo los limites del barrido para ST.

--- Software/Sandbox/Integracion numérica/chequeos_ODE/comparacion_taylor_barrido_en_b.py
+++ Software/Sandbox/Integracion numérica/chequeos_ODE/comparacion_taylor_barrido_en_b.py
@@ -12,12 +12,14 @@ from funciones_int import integrador
 from funciones_taylor import Taylor_HS, Taylor_ST
 
 #%%
-bs=np.array([0.01, 0.02, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 1, 2])
-bs=np.array([0.08,0.1, 0.2, 0.3, 0.4, 0.5, 1, 2])
 #bs=np.linspace(0.01,2,100)
-error_b=np.zeros(len(bs))
-omega_m=0.24
-H0=73.48
+bs=np.array([0.01, 0.02, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 1, 2]) #HS
+
+bs=np.array([0.08,0.1, 0.2, 0.3, 0.4, 0.5, 1, 2]) #ST
+
+error_b = np.zeros(len(bs))
+omega_m = 0.24
+H0 = 73.48
 
 
 #%% Hu-Sawicki n=2
@@ -25,24 +27,33 @@ for i, b in enumerate(bs):
     print(i)
     params_fisicos = [omega_m,b,H0]
 
-    zs, H_ode = integrador(params_fisicos, cantidad_zs=int(10**5), max_step=10**(-5),model='HS',n=2)
+#    zs, H_ode = integrador(params_fisicos, cantidad_zs=int(10**5), max_step=10**(-4), model='HS',n=1)
+#    H_taylor = Taylor_HS(zs,omega_m,b,H0)
+
+    zs, H_ode = integrador(params_fisicos, cantidad_zs=int(10**4), max_step=10**(-5),model='HS',n=2)
     H_taylor = Taylor_ST(zs,omega_m,b,H0)
 
     error_b[i] = 100*np.abs(np.mean(1-(H_taylor/H_ode)))
 #Tarda 1h y 20 aprox
+
 #%%
 %matplotlib qt5
 plt.close()
 plt.figure()
-plt.title('Error Porcentual para Hu-Sawicki n=2')
-plt.yscale('log')
 plt.grid(True)
-plt.xlabel('b')
-plt.ylabel('Error Porcentual')
+#plt.title('Error Porcentual para Hu-Sawicki n=1',fontsize=15)
+plt.title('Error Porcentual para Starobinsky n=1',fontsize=15)
+
+plt.xlabel('b',fontsize=13)
+plt.ylabel('Error Porcentual',fontsize=13)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.yscale('log')
+
 plt.plot(bs,error_b,label='Error en b')
-plt.legend(loc='best')
+plt.legend(loc='best',prop={'size': 12})
 plt.show()
-error_b
+#error_b
 
 #%%
 #b=0.5
