068000b1dcb375a6cb7adea174978375e01802a1
matiasleize <matias.leize@hotmail.com>
2021-08-31 16:20:38 -0300

    HS CC+SN solo Basilakos.

---
+++ Software/Estadística/MCMC/HS/CC+SN_taylor/4params/analisis_cadenas_4params.py
@@ -0,0 +1,36 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import emcee
+import sys
+import os
+
+from pc_path import definir_path
+path_git, path_datos_global = definir_path()
+
+os.chdir(path_git)
+sys.path.append('./Software/Funcionales/Clases')
+from funciones_graficador import Graficador
+
+#%% Importo los mínimos del chi2
+os.chdir(path_git+'/Software/Estadística/Resultados_simulaciones/')
+with np.load('valores_medios_HS_CC+SN_4params_taylor.npz') as data:
+    sol = data['sol']
+
+#%% Importo las cadenas
+os.chdir(path_datos_global+'/Resultados_cadenas/')
+filename = "sample_HS_CC+SN_4params_taylor.h5"
+reader = emcee.backends.HDFBackend(filename)
+
+# Algunos valores
+tau = reader.get_autocorr_time()
+burnin = int(2 * np.max(tau))
+thin = int(0.5 * np.min(tau))
+#%%
+%matplotlib qt5
+burnin=100
+thin=10
+analisis = Graficador(reader, ['$M_{abs}$','$\Omega_{m}$','b','$H_{0}$'],'SNIA + CC (HS) Taylor')
+analisis.graficar_contornos(sol, discard=burnin, thin=thin, poster=False,color='r')
+#%%
+analisis.graficar_cadenas()
+analisis.reportar_intervalos(sol)
