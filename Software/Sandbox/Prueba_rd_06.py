fd9cb855c794ea04cea19bceb4f870f4f6d679c7
matiasleize <matias.leize@hotmail.com>
2021-09-09 14:31:09 -0300

    Comparamos distintas maneras de calcular rd (agregamos el metodo de Augusto).

--- Software/Sandbox/Prueba_rd_06.py
+++ Software/Sandbox/Prueba_rd_06.py
@@ -7,11 +7,13 @@ import numpy as np
 from numpy.linalg import inv
 from matplotlib import pyplot as plt
 import time
+from numba import jit
 import camb
 from camb import model, initialpower
 from scipy.integrate import solve_ivp
 from scipy.integrate import cumtrapz as cumtrapz
 from scipy.integrate import simps as simps
+from scipy.integrate import quad as quad
 from scipy.interpolate import interp1d
 from scipy.constants import c as c_luz #metros/segundos
 c_luz_km = c_luz/1000
@@ -37,15 +39,12 @@ def H_LCDM_rad(z, omega_m, H_0):
     H = H_0 * np.sqrt(omega_r * (1 + z)**4 + omega_m * (1 + z)**3 + omega_lambda)
     return H
 
-def r_drag(omega_m,H_0,wb = 0.0225, int_z=True): #wb x default tomo el de BBN.
+def r_drag_viejo(omega_m,H_0,wb = 0.0225, int_z=True): #wb x default tomo el de BBN.
     #Calculo del rd:
     h = H_0/100
     zd = zdrag(omega_m,H_0)
     #R_bar = 31500 * wb * (2.726/2.7)**(-4)
     R_bar = wb * 10**5 / 2.473
-
-#omega_b va como h^(-2)
-#wb=omega_b * h**2 no depende de h
 
     #Integral logaritmica
     zs_int_log = np.logspace(np.log10(zd),13,int(10**5))
@@ -56,6 +55,33 @@ def r_drag(omega_m,H_0,wb = 0.0225, int_z=True): #wb x default tomo el de BBN.
     rd_log = simps(integrando_log,zs_int_log)
     return rd_log
 
+@jit
+def integrand(z, Om_m_0, H_0, wb):
+    R_bar = wb * 10**5 / 2.473
+
+    Om_r = 4.18343*10**(-5) / (H_0/100)**2
+    Om_Lambda = 1 - Om_m_0 - Om_r
+    H = H_0 * ((Om_r * (1 + z)**4 + Om_m_0 * (1 + z)**3 + Om_Lambda) ** (1/2))
+    return c_luz_km/(H * (3*(1 + R_bar*(1+z)**(-1)))**(1/2))
+
+def r_drag(omega_m,H_0,wb = 0.0225, int_z=True): #wb x default tomo el de BBN.
+    #Calculo del rd:
+    h = H_0/100
+    zd = zdrag(omega_m,H_0)
+    #R_bar = 31500 * wb * (2.726/2.7)**(-4)
+    R_bar = wb * 10**5 / 2.473
+
+
+   # Calculo del rd:
+    zd = zdrag(omega_m, H_0)
+    # zd = 1000
+    R_bar = wb * 10**5 / 2.473
+
+    rd_log, _ = quad(lambda z: integrand(z, omega_m, H_0, wb), zd, np.inf)
+
+    return rd_log
+
+
 def r_drag_camb(omega_m,H_0,wb = 0.0225):
     pars = camb.CAMBparams()
     h = (H_0/100)
@@ -65,22 +91,30 @@ def r_drag_camb(omega_m,H_0,wb = 0.0225):
     #print('Derived parameter dictionary: %s'%results.get_derived_params()['rdrag'])
     return rd
 
+omega_m =0.25
+H_0 = 70
+r_drag(omega_m,H_0)
+r_drag_viejo(omega_m,H_0)
+print((1-(r_drag(omega_m,H_0)/r_drag_viejo(omega_m,H_0))))
+#%%
 #Ref 15 de caro
 omega_m = 0.31
 h = 0.67
 wb = 0.048*h**2
 r_drag_camb(omega_m,h*100,wb=wb) #148.66089043939596
-r_drag(omega_m,h*100,wb=wb) #147.52197078103742
+r_drag_viejo(omega_m,h*100,wb=wb) #147.52197078103742
+r_drag(omega_m,h*100,wb=wb)
 #Deberia ser 148.69 Mpc
 print((1-(r_drag(omega_m,h*100,wb=wb)/r_drag_camb(omega_m,h*100,wb=wb)))*100)
-
+print((1-(r_drag(omega_m,h*100,wb=wb)/r_drag_viejo(omega_m,h*100,wb=wb)))*100)
 
 #Ref 16 de caro
 omega_m = 0.31
 h = 0.676
 wb = 0.022
 r_drag_camb(omega_m,h*100,wb=wb) #147.59690329061232
-r_drag(omega_m,h*100,wb=wb) #146.48214042341743
+r_drag_viejo(omega_m,h*100,wb=wb) #146.48214042341743
+r_drag(omega_m,h*100,wb=wb)
 #Deberia ser 147,78 Mpc
 print((1-(r_drag(omega_m,h*100,wb=wb)/r_drag_camb(omega_m,h*100,wb=wb)))*100)
 
@@ -89,7 +123,8 @@ omega_m = 0.25
 h = 0.7
 wb = 0.044*(h**2)
 r_drag_camb(omega_m,h*100,wb=wb) #153.41991935756414
-r_drag(omega_m,h*100,wb=wb) #152.31309641483853
+r_drag_viejo(omega_m,h*100,wb=wb) #152.31309641483853
+r_drag(omega_m,h*100,wb=wb)
 #Deberia ser 153.44 Mpc
 print((1-(r_drag(omega_m,h*100,wb=wb)/r_drag_camb(omega_m,h*100,wb=wb)))*100)
 
@@ -144,6 +179,7 @@ print((1-(r_drag(omega_m,h*100,wb=wb)/r_drag_camb(omega_m,h*100,wb=wb)))*100)
 omega_m = 0.286
 h = 0.7
 wb = 0.047*(h**2)
+r_drag_camb(omega_m,h*100,wb=wb) #147.12861165170563
 r_drag_camb(omega_m,h*100,wb=wb) #147.12861165170563
 r_drag(omega_m,h*100,wb=wb) #146.06953903165717
 #Deberia ser 147.15
