d95ed1cf2f4c0ba7a2b85c0620e696b87f9f7f0c
matiasleize <matias.leize@hotmail.com>
2021-09-15 18:36:25 -0300

    Estudio distintos burnin.

---
+++ Software/Estadística/MCMC/Paper/HS/CC+SN+BAO/4params/analisis_cadenas_4params_burnin.py
@@ -0,0 +1,35 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import emcee
+import sys
+import os
+
+from pc_path import definir_path
+path_git, path_datos_global = definir_path()
+
+os.chdir(path_git)
+sys.path.append('./Software/Funcionales/Clases')
+from funciones_graficador import Graficador
+
+#%% Importo las cadenas
+os.chdir(path_datos_global+'/Resultados_cadenas/Paper/HS')
+filename = "sample_HS_CC+SN+BAO_4params.h5"
+reader = emcee.backends.HDFBackend(filename)
+
+# Algunos valores
+tau = reader.get_autocorr_time()
+#burnin = int(2 * np.max(tau))
+sample = reader.get_chain()
+len(sample)
+burnin= burnin=int(0.2*len(sample[:,0]))
+thin = int(0.5 * np.min(tau))
+burnin
+#%%
+%matplotlib qt5
+analisis = Graficador(reader, ['$M_{abs}$', '$\Omega_{m}^{\Lambda CDM}$','b','$H_{0}^{\Lambda CDM}$'],
+                    'HS (SnIA + CC + BAO)')
+
+analisis.graficar_contornos(discard=burnin, thin=thin, poster=False,color='r')
+ #%%
+analisis.graficar_cadenas()
+analisis.reportar_intervalos()
---
+++ Software/Estadística/MCMC/Paper/HS/CC+SN+BAO/4params/analisis_cadenas_4params_deriv_burnin.py
@@ -0,0 +1,28 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import emcee
+import sys
+import os
+
+from pc_path import definir_path
+path_git, path_datos_global = definir_path()
+
+os.chdir(path_git)
+sys.path.append('./Software/Funcionales/Clases')
+from funciones_graficador import Graficador
+
+#%% Importo las cadenas
+os.chdir(path_datos_global+'/Resultados_cadenas/Paper/HS')
+#os.chdir(path_datos_global+'/Resultados_cadenas')
+with np.load('sample_HS_CC+SN+BAO_4params_deriv.npz') as data:
+    ns = data['new_samples']
+
+burnin=int(0.2*len(ns[:,0]))
+burnin
+ns = ns[burnin:,:]
+#%%
+%matplotlib qt5
+analisis = Graficador(ns, ['$M_{abs}$','$\Omega_{m}$','b','$H_{0}$'],'HS (SnIA + CC + BAO)')
+analisis.graficar_cadenas_derivs()
+analisis.graficar_contornos(poster=False,color='r')
+analisis.reportar_intervalos()
