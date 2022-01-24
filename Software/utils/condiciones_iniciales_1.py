d101a36f4f3ab7e8f93d32376f6d5275c81fb65b
matiasleize <matias.leize@hotmail.com>
2022-01-23 22:33:58 -0300

    Limpio las funciones y elimino la opcion de Starobinsky

--- Software/utils/condiciones_iniciales_1.py
+++ Software/utils/condiciones_iniciales_1.py
@@ -13,18 +13,26 @@ import os
 import git
 path_git = git.Repo('.', search_parent_directories=True).working_tree_dir
 path_datos_global = os.path.dirname(path_git)
-os.chdir(path_git)
-os.sys.path.append('./Software/utils/')
-from cambio_parametros import params_fisicos_to_modelo_HS
+#os.chdir(path_git)
+#os.sys.path.append('./Software/utils/')
+#from cambio_parametros import params_fisicos_to_modelo_HS
 
-def z_condicion_inicial(params_fisicos,eps=10**(-10)):
-    [omega_m,b,_] = params_fisicos
+def z_condicion_inicial(params_fisicos, eps=10**(-10)):
+    '''
+    z_i para el sistema de ecuaciones de Odintsov
+    x0 = -(1/3) * np.log((-omega_l/omega_m)*((np.log(eps)/beta)+2))
+    z0 = np.exp(-x0)-1    
+    '''
+ 
+    [omega_m, b, _] = params_fisicos
     beta = 2/b
     omega_l = 1 - omega_m
-    z0 = (2 * omega_l*(-np.log(eps)-2*beta)/(beta*omega_m))**(1/3) - 1
-    return z0
+    
+    #Defino el z inicial
+    zi = (2 * omega_l*(-np.log(eps)-2*beta)/(beta*omega_m))**(1/3) - 1
+    return zi
 
-def condiciones_iniciales(omega_m, b, z0=30, n=1, model='HS',CI_aprox=True):
+def condiciones_iniciales(params_fisicos, zi = 30, model = 'HS', CI_aprox=True):
     '''
     Calculo las condiciones iniciales para el sistema de ecuaciones diferenciales
     para el modelo de Hu-Sawicki y el de Starobinsky n=1
@@ -33,121 +41,109 @@ def condiciones_iniciales(omega_m, b, z0=30, n=1, model='HS',CI_aprox=True):
     las condiciones iniciales con H0. Además, como el output son adminensionales
     podemos tomar c=1 (lo chequeamos en el papel).
     '''
-    R = sym.Symbol('R')
-    Lamb = 3 * (1-omega_m)
 
+    [omega_m, b, _] = params_fisicos
 
-    if model=='EXP':
-        #Defino el z inicial
+    z = sym.Symbol('z')
+    E = (omega_m*(1+z)**3 + (1-omega_m))**(0.5)
+
+    if (model=='EXP' or model=='Odintsov'):
         omega_l = 1-omega_m
-        beta = 2/b
-        #eps = 10**(-9)
-        #x0 = -(1/3) * np.log((-omega_l/omega_m)*((np.log(eps)/beta)+2))
-        #z0 = np.exp(-x0)-1
-
-
-        z = sym.Symbol('z')
-        H = (omega_m*(1+z)**3 + omega_l)**(0.5)
 
         tildeR = 2 + (omega_m/(2*(1 - omega_m))) * (1+z)**3
 
-        tildeR_ci=sym.lambdify(z,tildeR)
-        H_ci=sym.lambdify(z,H)
+        tildeR_ci = sym.lambdify(z,tildeR)
+        E_ci = sym.lambdify(z,E)
 
-        tildeR0=tildeR_ci(z0)
-        E0 = H_ci(z0) #Esta ya noramlizado por H0!
+        tildeR_i = tildeR_ci(zi)
+        E0 = E_ci(zi) #Esta ya noramlizado por H0!
 
-        return[E0,tildeR0]
+        return[E0, tildeR_i]
 
     elif model=='HS':
-        c1,c2 = params_fisicos_to_modelo_HS(omega_m,b,n)
-        R_HS = 2 * Lamb * c2/c1
-        R_0 = R_HS #No confundir con R0 que es R en la CI!
+        R = sym.Symbol('R')
+        Lamb = 3 * (1-omega_m)
 
-        #Calculo F. Ambas F dan las mismas CI para z=z0 :)
-        #F = R - ((c1*R)/((c2*R/R_HS)+1))
-        F = R - 2 * Lamb * (1 - 1/ (1 + (R/(b*Lamb)**n)) )
-    elif model=='ST':
-        #lamb = 2 / b
-        R_ST = Lamb * b
-        R_0 = R_ST #No confundir con R0 que es R en la CI!
+        #c1,c2 = params_fisicos_to_modelo_HS(omega_m,b)
+        #R_HS = 2 * Lamb * c2/c1
+        R_HS = 6 * c_luz_km**2 * omega_m / (7800 * (8315)**2) 
 
-        #Calculo F.
-        F = R - 2 * Lamb * (1 - 1/ (1 + (R/(b*Lamb)**2) ))
+        #En el codigo de Augusto:
+        #R_0 = 3 * (1-omega_m) * b (No me cierra)
 
-    #Calculo las derivadas de F
-    F_R = sym.diff(F,R) #saué el sym.simplify para que ande el modelo exp!
-    F_2R = sym.diff(F_R,R)
+        R_0 = R_HS #No confundir con R_i que es R en la CI!
 
-    z = sym.Symbol('z')
-    H = (omega_m*(1+z)**3 + (1-omega_m))**(0.5)
-    #H_z = ((1+z)**3 *3 * omega_m)/(2*(1+omega_m*(-1+(1+z)**3))**(0.5))
-    H_z = sym.simplify(sym.diff(H,z))
+        #Calculo F
+        F = R - 2 * Lamb * (1 - 1/ (1 + (R/(b*Lamb))) )
+   
+        #Calculo las derivadas de F
+        F_R = sym.diff(F,R) #saqué el sym.simplify para que ande el modelo exp en su momento,
+                            # pero ahora se podría agregar
+        F_2R = sym.diff(F_R,R)
 
-    Ricci = (12*H**2 + 6*H_z*(-H*(1+z)))
-    Ricci_t=sym.simplify(sym.diff(Ricci,z)*(-H*(1+z)))
+        E_z = sym.simplify(sym.diff(E,z))
 
-    Ricci_ci=sym.lambdify(z,Ricci)
-    Ricci_t_ci=sym.lambdify(z,Ricci_t)
-    H_ci=sym.lambdify(z,H)
-    H_z_ci=sym.lambdify(z,H_z)
-    F_ci=sym.lambdify(R,F)
-    F_R_ci=sym.lambdify(R,F_R)
-    F_2R_ci=sym.lambdify(R,F_2R)
+        #Como hay una independencia con H0 en los resultados finales, defino H=E para
+        # que en el resultado final den bien las unidades
+        H = E
+        H_z = E_z
 
-    R0=Ricci_ci(z0)
-    #Ricci_t_ci(z0)
-    #H_ci(z0) #Chequie que de lo esperado x Basilakos
-    #H_z_ci(z0) #Chequie que de lo esperado x Basilakos
-    #F_ci(R0) # debe ser simil a R0-2*Lamb
-    #F_R_ci(R0) # debe ser simil a 1
-    #F_2R_ci(R0) # debe ser simil a 0
+        Ricci = (12*H**2 + 6*H_z*(-H*(1+z)))
+        Ricci_t = sym.simplify(sym.diff(Ricci,z)*(-H*(1+z)))
 
+        Ricci_ci = sym.lambdify(z,Ricci)
+        Ricci_t_ci = sym.lambdify(z,Ricci_t)
+        H_ci = sym.lambdify(z,H)
+        H_z_ci = sym.lambdify(z,H_z)
+        F_ci = sym.lambdify(R,F)
+        F_R_ci = sym.lambdify(R,F_R)
+        F_2R_ci = sym.lambdify(R,F_2R)
 
-    if CI_aprox == True:
-        #Hibrid initial conditions
-        x0 = Ricci_t_ci(z0)*F_2R_ci(R0) / (H_ci(z0)*F_R_ci(R0))
-        y0 = F_ci(R0) / (6*(H_ci(z0)**2)*F_R_ci(R0))
-        v0 = R0 / (6*H_ci(z0)**2)
-        w0 = 1+x0+y0-v0
-        r0 = R0/R_0
-    else:
-        #LCDM initial conditions
-        x0 = 0
-        y0 = (R0  - 2 * Lamb) / (6*H_ci(z0)**2)
-        v0 = R0 / (6*H_ci(z0)**2)
-        w0 = 1+x0+y0-v0
-        r0 = R0/R_0
-    return[x0,y0,v0,w0,r0]
+        R_i = Ricci_ci(zi)
+        #H_ci(zi) #Chequie que de lo esperado x Basilakos
+        #H_z_ci(zi) #Chequie que de lo esperado x Basilakos
+
+        if CI_aprox == True: #Hibrid initial conditions
+            xi = Ricci_t_ci(zi) * F_2R_ci(R_i) / (H_ci(zi) * F_R_ci(R_i))
+            yi = F_ci(R_i) / (6 * (H_ci(zi)**2) * F_R_ci(R_i))
+            vi = R_i / (6 * H_ci(zi)**2)
+            wi = 1 + xi + yi - vi
+            ri = R_i / R_0
+
+        else: #LCDM initial conditions
+            xi = 0
+            yi = (R_i  - 2 * Lamb) / (6 * H_ci(zi)**2)
+            vi = R_i / (6 * H_ci(zi)**2)
+            wi = 1 + xi + yi - vi
+            ri = R_i / R_0
+
+        return[xi,yi,vi,wi,ri]
 
 #%%
 if __name__ == '__main__':
     omega_m = 0.2
     b = 0.6
-    params_fisicos = [omega_m,b,_]
-    print(z_condicion_inicial(params_fisicos,eps=10**(-10)))
+    params_fisicos = [omega_m, b, 0]
+    print(z_condicion_inicial(params_fisicos, eps=10**(-10)))
     #%%
     H0 = 73.48
-    z0 = 30
-    cond_iniciales=condiciones_iniciales(omega_m,b,z0=z0,model='HS')
-    print(cond_iniciales)
-    cond_iniciales=condiciones_iniciales(omega_m,b,z0=z0,model='ST')
+    zi = 30
+    cond_iniciales = condiciones_iniciales(params_fisicos, zi=zi, model='HS')
     print(cond_iniciales)
     #%%
     bs = np.arange(0.2,1.1,0.1)
     omegas = np.arange(0.2,0.51,0.01)
     output = np.zeros((len(bs),len(omegas)))
-    bs
-    omegas
     for i, b in enumerate(bs):
         for j, omega in enumerate(omegas):
-            cond_iniciales=condiciones_iniciales(omega_m=omega,b=b,z0=3,
+            params_fisicos = [omega_m,b,0]
+            cond_iniciales=condiciones_iniciales(params_fisicos,zi=3,
                             model='EXP')
             output[i,j] = 2 * cond_iniciales[1]/b #lo convierto en r para comparar
-    np.savetxt('2darray.csv', output, delimiter=',', fmt='%1.2f')
+    #np.savetxt('2darray.csv', output, delimiter=',', fmt='%1.2f')
     output
     #%%
-    cond_iniciales=condiciones_iniciales(omega_m,b,z0=z0,model='HS')
-    cond_iniciales_1=condiciones_iniciales(omega_m,b,z0=z0,model='HS',CI_aprox=False)
-    print(cond_iniciales)
-    print(cond_iniciales_1)
+    cond_iniciales_hibrid = condiciones_iniciales(params_fisicos, zi=zi, model='HS', CI_aprox=True)
+    cond_iniciales_LCDM = condiciones_iniciales(params_fisicos, zi=zi, model='HS', CI_aprox=False)
+    print(cond_iniciales_hibrid)
+    print(cond_iniciales_LCDM)
--- Software/utils/int_sist_1.py
+++ Software/utils/int_sist_1.py
@@ -1,3 +1,15 @@
+'''
+Integra el modelo de Hu-Sawicki con el modelo de De la Cruz et al. y el modelo exponencial
+utilizando el sistema de ecuaciones de Odintsov. Las CI son distintas para ambos modelos
+(ver script int_sist_1.py). 
+
+Tarea: Ver cuanto tarda integrar HS con este sistema en comparacion con Odintsov y cuanta
+diferencia hay.
+
+El int_2 (que esta en la carpeta test) solo cambia en como se calcula eta (alfa en el codigo).
+
+Tarea: Probar si es mas rapido eta_1 o eta_2.
+'''
 import time
 import numpy as np
 from scipy.integrate import solve_ivp
@@ -15,27 +27,30 @@ os.sys.path.append('./Software/utils/')
 from condiciones_iniciales_1 import condiciones_iniciales, z_condicion_inicial
 from cambio_parametros import params_fisicos_to_modelo_HS
 from LambdaCDM import H_LCDM
-from taylor import Taylor_HS, Taylor_ST
+from taylor import Taylor_HS
 #%%
 
-def dX_dz(z, variables, params_modelo, model='HS'):
+def dX_dz(z, variables, params_fisicos, model='HS'):
     '''
     Sistema de ecuaciones a resolver pot la función Integrador.
     Parameters:
-        params_modelo: list
-            lista de n parámetros, donde los primeros n-1 elementos son los
-            parametros del sistema, mientras que el útimo argumento especifica el modelo
-            en cuestión, matemáticamente dado por la función \Gamma.
+        params_fisicos: list
+            #lista de n parámetros, donde los primeros n-1 elementos son los
+            #parametros del sistema, mientras que el útimo argumento especifica el modelo
+            #en cuestión, matemáticamente dado por la función \Gamma.
         model: string
             Modelo que estamos integrando.
     Returns: list
-        Set de ecuaciones diferenciales para las variables (x,y,v,w,r).
+        Set de ecuaciones diferenciales para las variables dinámicas.
     '''
+    
+    [omega_m, b, _] = params_fisicos
+
     if model == 'EXP':
         E = variables[0]
         tildeR = variables[1]
 
-        [omega_m,beta] = params_modelo
+        beta = 2/b
         omega_l = 1 - omega_m
 
         s0 = omega_l * tildeR/E - 2 * E
@@ -43,52 +58,35 @@ def dX_dz(z, variables, params_modelo, model='HS'):
 
         return [s0,s1]
 
-    else:
+    elif model == 'HS': #Para el modelo de Hu-Sawicki
         x = variables[0]
         y = variables[1]
         v = variables[2]
         w = variables[3]
         r = variables[4]
 
-        if model == 'ST': #Para el modelo de Starobinsky
-            [lamb,N] = params_modelo
-            if N==1:
-                gamma = lambda r,lamb: -(r**2 + 1)*(2*lamb*r - (r**2 + 1)**2)/(2*lamb*r*(3*r**2 - 1))
-                G = gamma(r,lamb) #Va como r^6/r^3 = r^3
-            else:
-                gamma = lambda r,lamb, N: (r**2 + 1)**(N + 2)*(-lamb*N*r*(r**2 + 1)**(-N - 1) + 1/2)/(lamb*N*r*(2*N*r**2 + r**2 - 1))
-                G = gamma(r,lamb,N)
-                #print('Guarda que estas poniendo n!=1')
-                pass
+        #Calculamos los parámetros del modelo
+        B, D = params_fisicos_to_modelo_HS(omega_m, b) # (c1,c2) = (B,D) from De la Cruz et al.
 
-        elif model == 'HS': #Para el modelo de Hu-Sawicki
-            [B,D,N] = params_modelo
-            if N==1:
-                gamma = lambda r,c1,c2: -(c1 - (c2*r + 1)**2)*(c2*r + 1)/(2*c1*c2*r)
-                G = gamma(r,B,D) #Va como r^3/r = r^2
-            elif N==2:
-                gamma = lambda r,c1,c2: (-2*c1*c2*r**3 - 2*c1*r + c2**3*r**6 + 3*c2**2*r**4 + 3*c2*r**2 + 1)/(2*c1*r*(3*c2*r**2 - 1))
-                G = gamma(r,B,D) #Va como r^6/r^3 = r^3
-            else:
-                gamma = lambda r,c1,c2,n: r**(-n)*(-c1*c2*n*r**(2*n) - c1*n*r**n + c2**3*r**(3*n + 1) + 3*c2**2*r**(2*n + 1) + 3*c2*r**(n + 1) + r)/(c1*n*(c2*n*r**n + c2*r**n - n + 1))
-                G = gamma(r,B,D,N)
-                #print('Guarda que estas poniendo n!=1')
-        else:
-            print('Elegir un modelo válido!')
-            pass
+        gamma = lambda r,c1,c2: -(c1 - (c2*r + 1)**2)*(c2*r + 1)/(2*c1*c2*r)
+        G = gamma(r,B,D) #Va como r^3/r = r^2
 
         s0 = (-w + x**2 + (1+v)*x - 2*v + 4*y) / (z+1)
         s1 = (- (v*x*G - x*y + 4*y - 2*y*v)) / (z+1)
         s2 = (-v * (x*G + 4 - 2*v)) / (z+1)
         s3 = (w * (-1 + x + 2*v)) / (z+1)
         s4 = (-(x * r * G)) / (1+z)
+
         return [s0,s1,s2,s3,s4]
 
+    else:
+        print('Choose a valid model!')
+        pass
 
-def integrador(params_fisicos,epsilon=10**(-10), n=1, cantidad_zs=int(10**5),
-                z_inicial=5, z_final=0,
+def integrador(params_fisicos, epsilon=10**(-10), cantidad_zs=int(10**5),
+                z_inicial=10, z_final=0,
                 sistema_ec=dX_dz, verbose=False, eval_data=False, z_data = None,
-                model='HS',method='RK45',rtol=1e-11, atol=1e-16):
+                model='HS',method='RK45', rtol=1e-11, atol=1e-16):
     '''
     Integración numérica del sistema de ecuaciones diferenciales entre
     z_inicial y z_final, dadas las condiciones iniciales de las variables
@@ -111,13 +109,12 @@ def integrador(params_fisicos,epsilon=10**(-10), n=1, cantidad_zs=int(10**5),
 
     t1 = time.time()
     [omega_m, b, H0] = params_fisicos
+
     if model=='EXP':
         z_ci = z_condicion_inicial(params_fisicos,epsilon)
-        #print(z_ci)
-        #if (np.isnan(z_ci)==True or z_ci<=0):
         beta = 2/b
-        params_modelo = [omega_m,beta]
-        cond_iniciales = condiciones_iniciales(omega_m, b, z0=z_ci, model='EXP')
+        cond_iniciales = condiciones_iniciales(params_fisicos, zi=z_ci, model='EXP')
+
         #Integramos el sistema
         zs_int = np.linspace(z_ci,z_final,cantidad_zs)
 
@@ -126,8 +123,8 @@ def integrador(params_fisicos,epsilon=10**(-10), n=1, cantidad_zs=int(10**5),
         xs_int = -np.log(1 + zs_int)
 
         sol = solve_ivp(sistema_ec, (x_ci,x_final),
-            cond_iniciales, t_eval=xs_int, args=(params_modelo,model),
-             rtol=rtol, atol=atol, method=method)
+            cond_iniciales, t_eval=xs_int, args=(params_fisicos, model),
+            rtol=rtol, atol=atol, method=method)
 
         xs_ode = sol.t[::-1]
         zs_ode = np.exp(-xs_ode)-1
@@ -150,46 +147,20 @@ def integrador(params_fisicos,epsilon=10**(-10), n=1, cantidad_zs=int(10**5),
             zs_final = z_data
             Hs_final = f(zs_final)
 
-        t2 = time.time()
-        if verbose == True:
-            print('Duración {} minutos y {} segundos'.format(int((t2-t1)/60),
-            int((t2-t1) - 60*int((t2-t1)/60))))
-        return zs_final,Hs_final
+    elif model=='HS':
+        #Calculo las condiciones cond_iniciales, eta
+        # y los parametros de la ecuación
+        cond_iniciales = condiciones_iniciales(params_fisicos, zi=z_inicial)
 
-    else:
-        if model=='HS':
-            #if n==1:
-                #max_step = 0.0001 #Resultado de la tesis (Deprecated)
-            #Calculo las condiciones cond_iniciales, eta
-            # y los parametros de la ecuación
-            cond_iniciales = condiciones_iniciales(omega_m, b, z0=z_inicial, n=n)
-
-            h = H0/100
-            R_HS = (omega_m * h**2)/(0.13*8315**2) #Mpc**(-2)
-            eta = c_luz_km * np.sqrt(R_HS/6) #(km/seg)/Mpc
-
-            c1, c2 = params_fisicos_to_modelo_HS(omega_m, b, n=n)
-
-            params_modelo = [c1,c2,n]
-
-        elif model=='ST':
-            #OJO Rs depende de H0, pero no se usa :) No como HS!! Cambiarlo en la tesis!!!
-            lamb = 2/b
-            #if n==1:
-                #max_step = 0.0001 #Resultado de la tesis (Deprecated)
-
-            cond_iniciales = condiciones_iniciales(omega_m, b, z0=z_inicial,model='ST')
-
-            R_ST = 3 * (H0/c_luz_km)**2 * (1-omega_m) * b #Mpc**(-2)
-            eta = c_luz_km * np.sqrt(R_ST/6) #(km/seg)/Mpc
-
-            params_modelo=[lamb, n]
+        h = H0/100
+        R_HS = (omega_m * h**2)/(0.13*8315**2) #Mpc**(-2)
+        eta = c_luz_km * np.sqrt(R_HS/6) #(km/seg)/Mpc
 
         if eval_data == False:
-            zs_int = np.linspace(z_inicial,z_final,cantidad_zs)
+            zs_int = np.linspace(z_inicial, z_final, cantidad_zs)
 
             sol = solve_ivp(sistema_ec, (z_inicial,z_final),
-                cond_iniciales, t_eval=zs_int, args=(params_modelo,model),
+                cond_iniciales, t_eval=zs_int, args=(params_fisicos, model),
                 rtol=rtol, atol=atol, method=method)
 
             if (len(sol.t)!=cantidad_zs):
@@ -198,26 +169,48 @@ def integrador(params_fisicos,epsilon=10**(-10), n=1, cantidad_zs=int(10**5),
                 print('Hay algo raro!')
         else:
             sol = solve_ivp(sistema_ec, (z_inicial,z_final),
-                cond_iniciales, t_eval=z_data.reverse(), args=(params_modelo,model),
+                cond_iniciales, t_eval=z_data.reverse(), args=(params_fisicos,model),
                 rtol=rtol, atol=atol, method=method)
 
             if (len(sol.t)!=len(z_data)):
                 print('Está integrando mal!')
             if np.all(z_data==sol.t)==False:
                 print('Hay algo raro!')
+
         #Calculamos el Hubble
-        zs = sol.t[::-1]
+        zs_final = sol.t[::-1]
+
         v=sol.y[2][::-1]
         r=sol.y[4][::-1]
-        Hs = eta * np.sqrt(r/v)
+        Hs_final = eta * np.sqrt(r/v)
 
-        t2 = time.time()
+    t2 = time.time()
 
-        if verbose == True:
-            print('Duración {} minutos y {} segundos'.format(int((t2-t1)/60),
-                  int((t2-t1) - 60*int((t2-t1)/60))))
+    if verbose == True:
+        print('Duración {} minutos y {} segundos'.format(int((t2-t1)/60),
+                int((t2-t1) - 60*int((t2-t1)/60))))
 
-        return zs, Hs
+    return zs_final, Hs_final
+    if model=='HS':
+        [omega_m, b, H0] = params_fisicos
+        #Calculo las condiciones cond_iniciales, eta
+        # y los parametros de la ecuacion
+        cond_iniciales = condiciones_iniciales(omega_m, b, z0=z_inicial, n=n)
+        alfa = H0*np.sqrt((1-omega_m)*b/2)
+        c1, c2 = params_fisicos_to_modelo_HS(omega_m, b, n=n)
+
+        params_modelo = [c1,c2,n]
+
+    #Integramos el sistema
+    zs_int = np.linspace(z_inicial,z_final,cantidad_zs)
+    sol = solve_ivp(sistema_ec, (z_inicial,z_final),
+        cond_iniciales, t_eval=zs_int, args=(params_modelo,model),
+        max_step=max_step)
+
+    if (len(sol.t)!=cantidad_zs):
+        print('Esta integrando mal!')
+    if np.all(zs_int==sol.t)==False:
+        print('Hay algo raro!')
 
 
 
@@ -237,7 +230,6 @@ def Hubble_teorico_1(params_fisicos, b_crit=0.15, all_analytic=False,
     elif model=='EXP': #b critico para el modelo exponencial
         log_eps_inv = -np.log10(epsilon)
         b_crit = (4 + omega_m/(1-omega_m)) / log_eps_inv
-        #method = 'LSODA' (Deprecated)
     else:
         pass
 
@@ -251,20 +243,20 @@ def Hubble_teorico_1(params_fisicos, b_crit=0.15, all_analytic=False,
             Hs_modelo = Taylor_HS(zs_modelo, omega_m, b, H0)
         elif (model=='HS') and (n==2):
             Hs_modelo = Taylor_ST(zs_modelo, omega_m, b, H0)
-        elif (model=='ST') and (n==1):
-            Hs_modelo = Taylor_ST(zs_modelo, omega_m, b, H0)
+        #elif (model=='ST') and (n==1):
+        #    Hs_modelo = Taylor_ST(zs_modelo, omega_m, b, H0)
         elif model=='EXP': #Devuelvo LCDM
             Hs_modelo = H_LCDM(zs_modelo, omega_m, H0)
 
     else: #Integro
         if eval_data == False:
-            zs_modelo, Hs_modelo = integrador(params_fisicos, epsilon=epsilon, n=n,
+            zs_modelo, Hs_modelo = integrador(params_fisicos, epsilon=epsilon,
                                     cantidad_zs=cantidad_zs,
                                     z_inicial=z_max, z_final=z_min, sistema_ec=sistema_ec,
                                     verbose=verbose, model=model,
                                      method=method,rtol=rtol, atol=atol)
         else:
-            zs_modelo, Hs_modelo = integrador(params_fisicos, epsilon=epsilon, n=n,
+            zs_modelo, Hs_modelo = integrador(params_fisicos, epsilon=epsilon,
                                     cantidad_zs=cantidad_zs,
                                     z_inicial=z_max, z_final=z_min, sistema_ec=sistema_ec,
                                     verbose=verbose, eval_data=True, z_data = z_data,
@@ -274,35 +266,15 @@ def Hubble_teorico_1(params_fisicos, b_crit=0.15, all_analytic=False,
 #%%
 if __name__ == '__main__':
     from matplotlib import pyplot as plt
-    # Hu-Sawicki (n=1)
-    b = 0.1
-    omega_m = 0.3
-    H0 = 73.48
 
     #%% Hu-Sawicki (n=1)
-    params_fisicos = [omega_m,b,H0]
+    params_fisicos = [0.3, 0.1, 73] # [omega_m, b, H0]
     zs_ode, H_HS = integrador(params_fisicos, verbose=True, model='HS')
     _, H_HS_1 = Hubble_teorico_1(params_fisicos, verbose=True, model='HS')
-
-    #%% Starobinsky (n=1)
-    b = 0.1
-    omega_m = 0.3
-    H0 = 73.48
-
-    #%%
-    params_fisicos = [omega_m,b,H0]
-    zs_ode, H_ST = integrador(params_fisicos, verbose=True, model='ST')
-    _, H_ST_1 = Hubble_teorico_1(params_fisicos, verbose=True, model='ST')
-
     #%% Exponencial
-    b = 2
-    omega_m = 0.5
-    H0 = 73.48
-
-    params_fisicos = [omega_m,b,H0]
+    params_fisicos = [0.3, 2, 73] # [omega_m, b, H0]
     zs_ode, H_EXP = integrador(params_fisicos, verbose=True, model='EXP')
     _, H_EXP_1 = Hubble_teorico_1(params_fisicos, verbose=True, model='EXP')
-
     #%% Graficamos todos los datos juntos
     #%matplotlib qt5
     plt.figure()
@@ -310,10 +282,8 @@ if __name__ == '__main__':
     plt.xlabel('z (redshift)')
     plt.ylabel('H(z) ((km/seg)/Mpc)')
     plt.plot(zs_ode,H_HS,'.',label='HS')
-    plt.plot(zs_ode,H_ST,'.',label='ST')
     plt.plot(zs_ode,H_EXP,'.',label='Exp')
-    plt.plot(zs_ode,H_LCDM(zs_ode,omega_m,H0),'.',label='LCDM')
+    plt.plot(zs_ode,H_LCDM(zs_ode,0.3,73),'.',label='LCDM') #H_LCDM(zs_ode,omega_m,H0)
     plt.legend(loc = 'best')
     plt.grid(True)
-
 # %%
