7fcfcbe63e9b68c9e297485eaa645ed2977b05be
matiasleize <matias.leize@hotmail.com>
2022-07-17 21:29:08 -0300

    Rename initial_conditions_1.py as initial_conditions.py

--- Software/utils/solve_sys.py
+++ Software/utils/solve_sys.py
@@ -25,7 +25,7 @@ path_datos_global = os.path.dirname(path_git)
 os.chdir(path_git)
 os.sys.path.append('./Software/utils/')
 
-from initial_conditions_1 import condiciones_iniciales, z_condicion_inicial
+from initial_conditions import condiciones_iniciales, z_condicion_inicial
 from change_of_parameters import params_fisicos_to_modelo_HS
 from LambdaCDM import H_LCDM
 from taylor import Taylor_HS
