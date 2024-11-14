c4e84048e5e34b5f630b0bfeb10674623859d850
matiasleize <matias.leize@hotmail.com>
2024-11-14 19:07:59 -0300

    Add constants file

---
+++ fr_mcmc/utils/constants.py
@@ -0,0 +1,16 @@
+# Constants for the project
+
+#Integration Constants
+NUM_Z_POINTS = int(10e5)
+Z_MIN = 0.0
+Z_MAX = 10.0
+
+#Radiation density
+OMEGA_R_0 = 2.47e-5
+
+#Baryonic matter density of BBN
+WB_BBN = 0.02218 #± 0.00055 #Baryon density (eq. 2.8 arXiv:2404.03002)
+
+#Kappa 
+KAPPA = 1.0
+#KAPPA = 8 * np.pi * G_newton / 3
