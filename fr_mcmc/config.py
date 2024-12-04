299838bfb7502982717566b14e7533e0d02fc2be
matiasleize <matias.leize@hotmail.com>
2024-12-03 22:45:14 -0300

    Miscellaneous

--- fr_mcmc/config.py
+++ fr_mcmc/config.py
@@ -23,9 +23,9 @@ os.chdir(path_git + '/configs/')
 ##yml_file = 'config_LCDM_PPS+CC+BAO_full.yml'
 
 ##HS
-#yml_file = 'config_HS_PPS.yml'
+yml_file = 'config_HS_PPS.yml'
 #yml_file = 'config_HS_CC.yml'
-yml_file = 'config_HS_BAO_full.yml'
+#yml_file = 'config_HS_BAO_full.yml'
 #yml_file = 'config_HS_PPS+CC.yml'
 #yml_file = 'config_HS_PPS+BAO_full.yml'
 #yml_file = 'config_HS_CC+BAO_full.yml'
